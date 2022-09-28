add86c75bd9c0e0f8a8a6427b8a817762c52d497
dlozon <lozo6089@kettering.edu>
2022-09-28 15:34:29 -0400

    added basic methods for controlling TurningMotors

--- src/main/java/frc/robot/subsystems/drivetrain/TurningMotor.java
+++ src/main/java/frc/robot/subsystems/drivetrain/TurningMotor.java
@@ -1,9 +1,39 @@
 package frc.robot.subsystems.drivetrain;
 
-public class TurningMotor {
-    
-    // Constructor method
-    public TurningMotor() {
+import frc.robot.wrappers.GenericPID;
 
+import static frc.robot.lists.PID_Values.*;
+import static frc.robot.lists.Constants.TURNING_MOTOR_RATIO;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class TurningMotor {
+    private CANSparkMax motor;
+    private GenericPID pid;
+
+    // Constructor method
+    public TurningMotor(int motorID) {
+        motor = new CANSparkMax(motorID, MotorType.kBrushless);
+
+        // initialize a GenericPID object with values from constants file
+        pid = new GenericPID(motor, ControlType.kPosition, 
+                             TURNING_MOTOR_P, TURNING_MOTOR_I, TURNING_MOTOR_D);
+    }
+ 
+    // Returns the current direction of the motor in degrees [0,360]
+    public double getCurrDir() {
+        return pid.getPosition() / TURNING_MOTOR_RATIO;
+    }
+
+    // Turns the motor by n degrees
+    public void turn(double degrees) {
+        pid.activate(getCurrDir() + (degrees * TURNING_MOTOR_RATIO));
+    }
+
+    // Turns the motor to n degrees
+    public void turnTo(double degrees) {
+        pid.activate(degrees * TURNING_MOTOR_RATIO);
     }
 }
